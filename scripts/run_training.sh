2ef4be26338cbf1b65a4d197ee3714af9f241c19
keisks <keisks5@gmail.com>
2021-12-21 14:45:06 -0800

    enable fp16 (apex)

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -9,8 +9,7 @@ CUDA_VISIBLE_DEVICES=4 python ../train_GeDi.py --task_name SST-2 \
   --overwrite_output_dir \
   --do_eval  \
   --do_train \
-  --logit_scale \
-  --data_dir ../data/AG-news  \
+  --data_dir ../data/AG-news_small  \
   --max_seq_length 192 \
   --overwrite_cache \
   --per_gpu_train_batch_size 4 \
@@ -25,5 +24,28 @@ CUDA_VISIBLE_DEVICES=4 python ../train_GeDi.py --task_name SST-2 \
   --save_steps 5000000000 \
   --code_0 false \
   --code_1 true \
+  --logit_scale \
   --fp16
 
+
+### no apex
+#CUDA_VISIBLE_DEVICES=4 python ../train_GeDi.py --task_name SST-2 \
+#  --overwrite_output_dir \
+#  --do_eval  \
+#  --do_train \
+#  --data_dir ../data/AG-news_small  \
+#  --max_seq_length 192 \
+#  --overwrite_cache \
+#  --per_gpu_train_batch_size 4 \
+#  --per_gpu_eval_batch_size  8 \
+#  --learning_rate $lr  \
+#  --num_train_epochs 1.0  \
+#  --output_dir ../topic_GeDi_retrained \
+#  --model_type gpt2  \
+#  --model_name_or_path gpt2-medium \
+#  --gen_weight $lambda_ \
+#  --logging_steps 500 \
+#  --save_steps 5000000000 \
+#  --code_0 false \
+#  --code_1 true \
+#  --logit_scale
